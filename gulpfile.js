beb94b424f7865e04f0b736a9c8854dd80847b5a
Will Spurgin <will.spurgin@gmail.com>
2016-10-31 13:05:39 -0500

    Minimum viable angular2 plus laravel app

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,19 @@ var elixir = require('laravel-elixir');
  |
  */
 
+var elixirTypscript = require('elixir-typescript-angular2');
+
 elixir(function(mix) {
     mix.sass('app.scss');
+    mix.styles('app.css', 'public/build/css/app.css', 'public/css');
+
+    mix.copy('node_modules/@angular', 'public/node_modules/@angular');
+    mix.copy('node_modules/core-js', 'public/node_modules/core-js');
+    mix.copy('node_modules/rxjs', 'public/node_modules/rxjs');
+    mix.copy('node_modules/systemjs', 'public/node_modules/systemjs');
+    mix.copy('node_modules/zone.js/', 'public/node_modules/zone.js/');
+    mix.copy('node_modules/reflect-metadata',
+      'public/node_modules/reflect-metadata');
+
+    mix.typescript(['main.ts', 'app.component.ts', 'app.module.ts'], './public/app');
 });
